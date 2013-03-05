eed82e5da00766b27f88b2d4f8bdae64a5d54ac0
Vladimir <rcheemann@hotmail.com>
2013-03-05 13:00:27 -0600

    Initial sync

---
+++ ex 163.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 163|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;robert heemann (THE GREAT VLADIMIR SKOVZKY)
+;EX 163: Design a Program that sorts you mail by date
